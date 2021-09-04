31f64931e665507906a0d2b53500b1b79effb56d
AHMET ARAPOĞLU <45310027+ahmetarapoglu@users.noreply.github.com>
2021-09-05 01:07:57 +0300

    update

---
+++ BookStore/Startup.cs
@@ -0,0 +1,77 @@
+using BookStore.Models;
+using BookStore.Models.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Data.Entity;
+//using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore
+{
+    public class Startup
+    {
+
+        private readonly IConfiguration configuration;
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(option => option.EnableEndpointRouting = false);
+
+            services.AddScoped<IBookstoreRepository<Author>, AuthorDbRepository>();
+            services.AddScoped<IBookstoreRepository<Book>, BookDbRepository>();
+            services.AddRazorPages();
+            services.AddDbContext<BookstoreDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("SqlCon"));
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+
+            app.UseStaticFiles();
+            app.UseMvc(route => {
+                route.MapRoute("default", "{controller=Book}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
+
